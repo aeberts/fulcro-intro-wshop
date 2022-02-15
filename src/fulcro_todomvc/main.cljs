061c3d677331a10e6a6996b1b6dc9f7cc5019407
Alex Eberts <alex.eberts@gmail.com>
2022-02-15 15:48:54 -0800

    Added log config to filter debug messages

--- src/fulcro_todomvc/main.cljs
+++ src/fulcro_todomvc/main.cljs
@@ -6,14 +6,21 @@
     [com.fulcrologic.fulcro.application :as app]
     [com.fulcrologic.fulcro.data-fetch :as df]
     [taoensso.timbre :as log]
+    [taoensso.sente     :as sente]
     [fulcro-todomvc.app :refer [app]]))
+
+;;(def log-whitelist #{})
+;;(def log-blacklist #{"io.undertow.websockets.*"})
 
 (defn ^:export start []
   (app/mount! app ui/Root "app")
   (df/load! app [:list/id 1] ui/TodoList
     {:target [:root/current-list]})
   (log/merge-config! {:output-fn prefix-output-fn
-                      :appenders {:console (console-appender)}}))
+                      :appenders {:console (console-appender)}
+                      :min-level :info
+                      ;:ns-filter {:allow log-whitelist :deny log-blacklist}
+                      }))
 
 (comment
   (app/set-root! app ui/Root {:initialize-state? true})
@@ -22,4 +29,5 @@
   (app/mount! app ui/Root "app" {:initialize-state? false})
   @(::app/state-atom app)
   (fws/stop! remote)
+  (deref sente/debug-mode?_)
   )
